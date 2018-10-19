a60751754ebedf937dc97479b4e4198be03dfbaf
Robert Snyder <r.snyder49@gmail.com>
2018-10-19 21:32:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,8 @@ class Application
     if req.path.match(/add/)
       add_item = req.params["item"]
       if @@items.include?(add_item)
-        @@cart << add_item 
+        @@cart << add_item
+        resp.write "added #{add_item}"
       else 
         resp.write "We don't have that item"
       end 
