96fd98562ce8df1a377e422311aff31aa9642f24
Robert Snyder <r.snyder49@gmail.com>
2018-10-19 21:28:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
+    if req.path.match(/add/)
+      
 
     if req.path.match(/items/)
       @@items.each do |item|
